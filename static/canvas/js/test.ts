561a1c6c214304384931e48be89a1ab473654253
esquembre <esquembre92@gmail.com>
2016-08-01 14:35:29 +0200

    added package.json file and dragula drag and drop

--- static/canvas/js/test.ts
+++
@@ -1,8 +0,0 @@
-import { RenderableElement } from './modules'
-
-
-var a: Array<number> = [1, 2, 3]
-var a2 = a.map(x => x + 1)
-
-var thing = new RenderableElement('a', 1, 1)
-thing.debug()
--- static/canvas/src/main.ts
+++ static/canvas/src/main.ts
@@ -1,17 +1,32 @@
 import { Panel } from './panels'
 
 declare var $: any
+declare var dragula: any
+console.log($)
 
 var $interface = $('#interface-container')
 var $canvas = $('#canvas-container')
 var $body = $('body')
 
-var panel = new Panel($interface, 0, 0)
-panel.render()
-panel.destroy()
+/** entry point */
+init()
+function init() {
+    configureInterface()
+}
 
-getComponents('/', x => console.log(x))
-getComponent('/blocks/test.html', x => addComponentToCanvas($(x.html), x.options))
+function configureInterface() {
+    // Drawing interface
+    var panel = new Panel($interface, 0, 0)
+    panel.render()
+    panel.destroy()
+    dragula([document.querySelector('#canvas-container')])
+
+    // Test nodes
+    getComponents('/', x => console.log(x))
+    getComponent('/blocks/test.html', x => addComponentToCanvas($(x.html), x.options))
+    getComponent('/blocks/test2.html', x => addComponentToCanvas($(x.html), x.options))
+    getComponent('/blocks/test.html', x => addComponentToCanvas($(x.html), x.options))
+}
 
 /** passes a list of the components (and directories) of a given path to the callback */
 function getComponents(path, callback) {
